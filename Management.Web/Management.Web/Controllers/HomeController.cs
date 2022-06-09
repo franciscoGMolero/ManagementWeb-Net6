53712097d943d57f5edc5c911a9c0396d9289ae6
FRANCISCO_GARCIA_MOLERO <francisco.garciamolero@unit4.com>
2022-06-09 23:56:42 +0200

    Update Layout

--- Management.Web/Management.Web/Controllers/HomeController.cs
+++ Management.Web/Management.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace Management.Web.Controllers
 {
+    // eg: Home/Index
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;

d1a829937fe324c58b4170d535b1122d051452c3
FRANCISCO_GARCIA_MOLERO <francisco.garciamolero@unit4.com>
2022-07-09 01:10:23 +0200

    fixed error in LeaeTypeRepository

--- Management.Web/Management.Web/Repositories/LeaveTypeRepository.cs
+++ Management.Web/Management.Web/Repositories/LeaveTypeRepository.cs
@@ -1,8 +1,9 @@
-﻿using Management.Web.Data;
+﻿using Management.Web.Contracts;
+using Management.Web.Data;
 
 namespace Management.Web.Repositories
 {
-    public class LeaveTypeRepository : GenericRepository<LeaveType>
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
     {
         public LeaveTypeRepository(ApplicationDbContext context) : base(context)
         {
